7381046398535a625ed0c9517703fc8b73543000
Gustav Sandström <gussan@omegapoint.se>
2019-04-05 11:56:29 +0200

    more logic to not reload a folder that exist. also some printout

--- target/get_data_scripts.py
+++ target/get_data_scripts.py
@@ -1,16 +1,17 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[17]:
 
 
 import json
 import shutil
 import os
 import requests
+import time
 
 
-# In[2]:
+# In[18]:
 
 
 def get_json_data(lat, lng):
@@ -25,37 +26,48 @@ def get_json_data(lat, lng):
         return None
 
 
-# In[3]:
+# In[22]:
 
 
-json_data = get_json_data(lng=106.20,lat=53.98)
-json_data
+json_data = get_json_data(lng=106.18,lat=54.00)
 
 
-# In[6]:
+# In[23]:
 
 
 def toString(long, lat):
     return "lng:" + long + "lat:" + lat
 
 def download_files(rel_dir, json_req):
-    if not os.path.exists(rel_dir):
-        os.makedirs(rel_dir)
+    try:
+        if not os.path.exists(rel_dir):
+            os.makedirs(rel_dir)
 
-    name = toString(json_data["location"]["lng"], json_data["location"]["lat"]) + "/"
+        name = toString(json_data["location"]["lng"], json_data["location"]["lat"]) + "/"
 
-    if not os.path.exists(rel_dir + name):
-        os.makedirs(rel_dir + name)
-    
-    for image in json_data["images"]:
-        response = requests.get(image["url"], stream=True)
-        file_name = rel_dir + name + "time:" + str(image["time"]) + ".png"
-        with open(file_name, 'wb') as out_file:
-            shutil.copyfileobj(response.raw, out_file)
-        del response
+        if os.path.exists(rel_dir + name):
+            print("you've already downloaded " + name + ". If you want to redo this, delete this directory.")
+            return
+        else:
+            os.makedirs(rel_dir + name)
+
+        print("downloading " + str(len(json_data["images"])) + " into folder " + name)
+        t = time.time()
+        for idx, image in enumerate(json_data["images"]):
+            if idx % 10 == 0:
+                print(str(idx) +  " images downloaded")
+            response = requests.get(image["url"], stream=True)
+            file_name = rel_dir + name + "time:" + str(image["time"]) + ".png"
+            with open(file_name, 'wb') as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+            del response
+        print("done. took " + str(time.time() - t) + " time")
+    except:
+        print("something went wrong. removing " + name)
+        shutil.rmtree(rel_dir + name)
 
 
-# In[7]:
+# In[ ]:
 
 
 download_files("./downloads/", json_data)
@@ -63,13 +75,13 @@ download_files("./downloads/", json_data)
 
 # ## Generate versioncontrollable artifacts
 
-# In[ ]:
+# In[8]:
 
 
 get_ipython().system("jupyter nbconvert --output-dir='./target' --to python get_data_scripts.ipynb")
 
 
-# In[ ]:
+# In[9]:
 
 
 get_ipython().system("jupyter nbconvert --output-dir='./target' --to html get_data_scripts.ipynb")
